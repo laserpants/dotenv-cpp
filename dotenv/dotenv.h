c0713840a996c7b1bed37b1fb5ecf8f8a073749e
Johannes Hildén <hildenjohannes@gmail.com>
2018-10-31 13:20:35 +0300

    add docblocks

--- dotenv/dotenv.h
+++ dotenv/dotenv.h
@@ -1,13 +1,59 @@
+///
+/// \file dotenv.h
+///
 #pragma once
 
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 
+///
+/// Utility class for loading environment variables from a `.env` file.
+///
+/// \code
+/// DATABASE_HOST=localhost
+/// DATABASE_USERNAME=user
+/// DATABASE_PASSWORD="antipasto"
+/// \endcode
+///
+/// \code
+/// // example.cpp
+/// #include <iostream>
+/// #include "dotenv/dotenv.h"
+///
+/// int main()
+/// {
+///     dotenv::init();
+///
+///     std::cout << std::getenv("DATABASE_USERNAME") << std::endl;
+///     std::cout << std::getenv("DATABASE_PASSWORD") << std::endl;
+///
+///     return 0;
+/// }
+/// \endcode
+///
+/// Compile and run the program, e.g. using,
+///
+/// \code
+/// c++ example.cpp -o example && ./example
+/// \endcode
+///
+/// and the output is:
+///
+/// \code
+/// user
+/// antipasto
+/// \endcode
+///
+/// \see https://github.com/laserpants/dotenv-cpp
+///
 class dotenv
 {
 public:
-    static const unsigned char DontOverwrite = 1 << 0;
+    dotenv() = delete;
+    ~dotenv() = delete;
+
+    static const unsigned char Preserve = 1 << 0;
 
     static const int OptionsNone = 0;
 
@@ -21,23 +67,54 @@ private:
     static std::string strip_quotes(const std::string& str);
 };
 
-void dotenv::init(const char* filename)
+///
+/// Read and initialize environment variables from the `.env` file, or a file
+/// specified by the \a filename argument.
+///
+/// \param filename a file to read environment variables from
+///
+inline void dotenv::init(const char* filename)
 {
     dotenv::do_init(OptionsNone, filename);
 }
 
-void dotenv::init(int flags, const char* filename)
+///
+/// Read and initialize environment variables using the provided configuration
+/// flags.
+///
+/// By default, if a name is already present in the environment, `dotenv::init()`
+/// will replace it with the new value. To preserve existing variables, you
+/// must pass the `Preserve` flag.
+///
+/// \code
+/// dotenv::init(dotenv::Preserve);
+/// \endcode
+///
+/// \param flags    configuration flags
+/// \param filename a file to read environment variables from
+///
+inline void dotenv::init(int flags, const char* filename)
 {
     dotenv::do_init(flags, filename);
 }
 
-std::string dotenv::getenv(const char* name, const std::string& def)
+///
+/// Wrapper for std::getenv() which also takes a default value, in case the
+/// variable turns out to be empty.
+///
+/// \param name the name of the variable to look up
+/// \param def  a default value
+///
+/// \returns the value of the environment variable \a name, or \a def if the
+///          variable is not set
+///
+inline std::string dotenv::getenv(const char* name, const std::string& def)
 {
     const char* str = std::getenv(name);
     return str ? std::string(str) : def;
 }
 
-void dotenv::do_init(int flags, const char* filename)
+inline void dotenv::do_init(int flags, const char* filename)
 {
     std::ifstream file;
     std::string line;
@@ -58,14 +135,14 @@ void dotenv::do_init(int flags, const char* filename)
             } else {
                 const auto name = line.substr(0, pos);
                 const auto val = strip_quotes(line.substr(pos + 1));
-                setenv(name.c_str(), val.c_str(), ~flags & dotenv::DontOverwrite);
+                setenv(name.c_str(), val.c_str(), ~flags & dotenv::Preserve);
             }
             ++i;
         }
     }
 }
 
-std::string dotenv::strip_quotes(const std::string& str)
+inline std::string dotenv::strip_quotes(const std::string& str)
 {
     const std::size_t len = str.length();
 
