981d72d650b06299687948874243696958a41a92
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2018-11-05 11:24:29 +0300

    Update dotenv.h

--- dotenv/dotenv.h
+++ dotenv/dotenv.h
@@ -1,4 +1,3 @@
-//
 // Copyright (c) 2018 Heikki Johannes Hildén <hildenjohannes@gmail.com>
 // 
 // All rights reserved.
@@ -29,7 +28,6 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
 
 ///
 /// \file dotenv.h
