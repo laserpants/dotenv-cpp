db805659584408f9291bd17d59386ffa2e0be5d6
Carsten Arnholm <arnholm@arnholm.org>
2022-01-08 19:49:14 +0100

    Add missing header <functional> (MSVC)

--- include/laserpants/dotenv/dotenv.h
+++ include/laserpants/dotenv/dotenv.h
@@ -38,6 +38,7 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <functional>
 
 ///
 /// Utility class for loading environment variables from a file.
@@ -166,8 +167,8 @@ inline std::string dotenv::getenv(const char* name, const std::string& def)
 int setenv(const char *name, const char *value, int overwrite)
 {
     int errcode = 0;
-    
-    if (!overwrite) 
+
+    if (!overwrite)
     {
         size_t envsize = 0;
         errcode = getenv_s(&envsize, NULL, 0, name);
